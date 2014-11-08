4532bb8742f757aad52ae778703c2173d958c286
Michael Todd <mikejtodd@gmail.com>
2014-11-08 11:08:12 -0600

    Added if statement for home page

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,6 @@ class Item < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   has_many :loans
-  belongs_to :user
+  belongs_to :loaner, :class_name => "User"
+  belongs_to :borrower, :class_name => "User"
 end
